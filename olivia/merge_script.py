4898d62afbe207c8dcf32b0184aa4d44d2b6617e
swuser <you@example.com>
2024-12-12 15:38:01 +0100

    merger script checks output file existence

--- olivia/merge_script.py
+++ olivia/merge_script.py
@@ -1,4 +1,6 @@
 import sys
+import shutil
+import os
 
 from olivia.histogram_functions import join_histograms_from_files
 from olivia.histogram_plot_functions import plot_histograms_from_file
@@ -8,4 +10,12 @@ if __name__ == "__main__":
     monfile  = sys.argv[1]
     monfiles = sys.argv[2:]
     
+    if not os.path.isfile(monfile):
+        # Copy first file that exists
+        for fname in monfiles:
+            monfiles = monfiles[1:]
+            if os.path.isfile(fname):
+                shutil.copyfile(fname, monfile)
+                break
+
     join_histograms_from_files([monfile]+monfiles, join_file=monfile)
